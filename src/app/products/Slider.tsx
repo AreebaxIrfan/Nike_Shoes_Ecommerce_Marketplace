9bdc0bc598ea020a9bcdda1ff5e4e4b5b05fc849
Areeba Irfan <161799302+AreebaxIrfan@users.noreply.github.com>
2025-09-29 22:40:24 +0500

    Update Slider.tsx

--- src/app/products/Slider.tsx
+++ src/app/products/Slider.tsx
@@ -19,27 +19,33 @@ const FilterSidebar = ({
   return (
     <aside
       className={`absolute sm:fixed md:static bg-white z-40 w-3/4 md:w-1/4 p-4 border-r transition-transform ${
-        isSidebarOpen ? 'translate-x-0' : '-translate-x-full'
+        isSidebarOpen ? "translate-x-0" : "-translate-x-full"
       } md:translate-x-0`}
     >
+      {/* Category List */}
       <ul className="space-y-2">
         {categories.map((category, index) => (
-          <li
-            key={index}
-            className={`cursor-pointer hover:underline ${
-              selectedCategory === category ? 'text-black font-semibold' : ''
-            }`}
-            onClick={() => setSelectedCategory(category)}
-          >
-            {category}
+          <li key={index}>
+            <button
+              type="button"
+              className={`w-full text-left cursor-pointer hover:underline ${
+                selectedCategory === category ? "text-black font-semibold" : ""
+              }`}
+              onClick={() => setSelectedCategory(category)}
+            >
+              {category}
+            </button>
           </li>
         ))}
       </ul>
+
       <hr className="my-4" />
+
+      {/* Sort Dropdown */}
       <div>
-        <h3 className="font-semibold cursor-pointer">Sort By</h3>
+        <h3 className="font-semibold">Sort By</h3>
         <select
-          className="border rounded p-1 text-sm mt-2"
+          className="border rounded p-1 text-sm mt-2 w-full"
           value={selectedSort}
           onChange={(e) => setSelectedSort(e.target.value)}
         >
@@ -53,6 +59,3 @@ const FilterSidebar = ({
 };
 
 export default FilterSidebar;
-
-
-
